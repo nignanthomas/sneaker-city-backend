e8f53d9024adbed6573dd8b98bd0679188fb859d
nignanthomas <nignanthomas@gmail.com>
2021-06-15 23:26:30 +0200

    - update postgres dialect with ssl

--- src/database/config.js
+++ src/database/config.js
@@ -15,7 +15,7 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
-      ssl: true,
+      ssl: { rejectUnauthorized: false },
     },
     logging: false,
   }
